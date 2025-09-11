1c6cdf3c7710af74fc573034b7c155f28716b6ca
Cocotterooo <fcocoterooo@gmail.com>
2025-09-11 10:04:55 +0200

    feat: update vercel import and add LocalStatus component to NavBar

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@
 import { defineConfig, envField, fontProviders } from 'astro/config';
 import tailwindcss from "@tailwindcss/vite";
 
-import vercel from '@astrojs/vercel/serverless';
+import vercel from '@astrojs/vercel';
 
 // https://astro.build/config
 export default defineConfig({
